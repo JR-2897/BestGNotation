a1f9630b9fd845945673446680df600f4620d3e2
Pierre VILLIERS <pierre.villiers@ynov.com>
2021-01-14 15:37:01 +0100

    Ajout valeur par défaut pour is_admin dans User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_initialize :init
+
+  def init
+    self.is_admin = false if self.is_admin.nil?
+  end
 end

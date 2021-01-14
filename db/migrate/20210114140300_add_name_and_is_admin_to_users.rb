c00465f2834326666264a4a3f20821179efb1465
JR-2897 <jean.ragueneau.pro@gmail.com>
2021-01-14 15:21:26 +0100

    Ajout authentification non fonctionnel

---
+++ db/migrate/20210114140300_add_name_and_is_admin_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameAndIsAdminToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :is_admin, :bool
+  end
+end

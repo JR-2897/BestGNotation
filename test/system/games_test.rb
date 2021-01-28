6bdbf367793d1c1a681fdee7d37fb19dafc92b56
JR-2897 <jean.ragueneau.pro@gmail.com>
2021-01-28 17:18:44 +0100

    Correction test

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class GamesTest < ApplicationSystemTestCase
    test "visiting the index and create a game" do
      visit games_path
-     assert_selector "h1", text: "Jeu"
+     save_and_open_screenshot
 
      click_on "Nouveau jeu"
 
@@ -11,16 +11,21 @@ class GamesTest < ApplicationSystemTestCase
      fill_in "Description", with "Test description"
      fill_in "Note", with 18.0
      fill_in "DatePublished", with "2019-03-16"
-     select "3", from "Category"
-     select "5", from "Studio"
-     select "4", from "Platform"
+     select "1", from "Category"
+     select "2", from "Studio"
+     select "1", from "Platform"
      save_and_open_screenshot
 
      click_on "Create Game"
      save_and_open_screenshot
 
-     assert_selector "h1", text: "Test - Nintendo Switch"
-     assert_selector "h2", text: "Steel Wool Studios"
+     assert_selector "h1", text: "Test - PS4"
+     assert_selector "h2", text: "Iros"
+     assert_selector "p", text: "Type : RPG"
+     assert_selector "p", text: "Description : Test description"
+     assert_selector "p", text: "Note du jeu : 18/20"
+     assert_selector "p", text: "Date de publication : 16-03-2019"
+     save_and_open_screenshot
 
    end
 end

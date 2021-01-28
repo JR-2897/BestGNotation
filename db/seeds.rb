438240e54b904ff5fab92691387a802a2bb97d27
JR-2897 <jean.ragueneau.pro@gmail.com>
2021-01-28 12:48:03 +0100

    Seed V2

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,13 @@ platforms = Platform.create([{platformName: 'PC'} ,{platformName: 'Xbox one'} ,{
 
 categories = Category.create([{categoryName: 'RPG'}, {categoryName: 'MMORPG'}, {categoryName: 'Stratégie'},
                 {categoryName: 'FPS'}, {categoryName: 'Horreur'}, {categoryName: 'VR'}, {categoryName: 'Combat'},
-                {categoryName: 'Réflexion'}, {categoryName: 'Aventure'}, {categoryName: 'Action'}, {categoryName: 'Simulation'}])
+                {categoryName: 'Réflexion'}, {categoryName: 'Aventure'}, {categoryName: 'Action'}, {categoryName: 'Simulation'},
+                {categoryName: 'Multijoueur'}])
 
-studios = Studio.create([{nameStudio: 'Bethesda Game Studios', dateCreated: '2001'}, {nameStudio: 'Blizzard Entertainment', dateCreated: '1991'},
-                            {nameStudio: 'Infinity Ward', dateCreated: '2003'}, {nameStudio: 'BioWare Edmonton Studio', dateCreated: '1995'},
-                            {nameStudio: 'Steel Wool Studios', dateCreated: '2013'}, {nameStudio: 'Skull Games Studio',dateCreated: '2019-10-10'} ])
+studios = Studio.create([{nameStudio: 'Bethesda Game Studios', dateCreated: '2001-01-01'}, {nameStudio: 'Blizzard Entertainment', dateCreated: '1991-01-01'},
+                            {nameStudio: 'Infinity Ward', dateCreated: '2003-01-01'}, {nameStudio: 'BioWare Edmonton Studio', dateCreated: '1995-01-01'},
+                            {nameStudio: 'Steel Wool Studios', dateCreated: '2013-01-01'}, {nameStudio: 'Skull Games Studio', dateCreated: '2019-10-10'},
+                            {nameStudio: 'Nintendo', dateCreated: '1889-01-01'}, {nameStudio: 'Motive Studios', dateCreated: '2015-01-01'} ])
 
 users = User.create([{first_name: 'Admin', last_name: 'Admin', email: 'admin@gmail.com',is_admin: true,
                          password: 'adminadmin', password_confirmation: 'adminadmin'}, # mdp: adminadmin
@@ -25,18 +27,42 @@ users = User.create([{first_name: 'Admin', last_name: 'Admin', email: 'admin@gma
                         {first_name: 'Test', last_name: 'test', email: 'test@test.com',is_admin: false,
                          password: '000000', password_confirmation: '000000'}])# mdp: 000000
 
-games = Game.create([{name: 'Elder Scroll 10 : Landersbram', description: 'Une nouvelle terre à découvrir et à dominer.', note: 5.0,
+games = Game.create([{name: 'The Elder Scrolls V: Skyrim', description: 'Sauver le monde du dragon Alduin le devoreur de monde.', note: 20.0,
                         datePublished: '2021-01-10', category: categories[0], studio: studios[0], platform: platforms[0] },
-                     {name: 'Mass Effect 3', description: "L'avenir des races intelligentes est entre les mains du Commandant Shepard.", note: 4.5,
+                     {name: 'Mass Effect 3', description: "L'avenir des races intelligentes est entre les mains du Commandant Shepard.", note: 18.0,
                         datePublished: '2020-07-10', category: categories[0], studio: studios[1], platform: platforms[0] },
-                        {name: 'Mass Effect 3', description: "L'avenir des races intelligentes est entre les mains du Commandant Shepard.", note: 4.5,
+                     {name: 'Mass Effect 3', description: "L'avenir des races intelligentes est entre les mains du Commandant Shepard.", note: 18.0,
                         datePublished: '2020-07-10', category: categories[0], studio: studios[1], platform: platforms[1] },
-                        {name: 'Mass Effect 3', description: "L'avenir des races intelligentes est entre les mains du Commandant Shepard.", note: 4.5,
+                     {name: 'Mass Effect 3', description: "L'avenir des races intelligentes est entre les mains du Commandant Shepard.", note: 18.0,
                         datePublished: '2020-07-10', category: categories[0], studio: studios[1], platform: platforms[2] },
-                        {name: 'Portal Arena', description: "Combatter pour obtenir la gloire de votre espèce dans l'arène de Portal Arena.", note: 5.0,
-                        datePublished: '2021-01-15', category: categories[6], studio: studios[5], platform: platforms[0] }
+                     {name: 'Portal Arena', description: "Combatter pour obtenir la gloire de votre espèce dans l'arène de Portal Arena.", note: 20.0,
+                        datePublished: '2021-01-15', category: categories[6], studio: studios[5], platform: platforms[0] },
+                     {name: 'Fallout 4', description: 'Retrouver votre fils dans un monde Post-apo.', note: 15.0,
+                        datePublished: '2015-11-10', category: categories[0], studio: studios[0], platform: platforms[0] },
+                     {name: 'Call of Duty: Warzone', description: 'Battle royal.', note: 15.0,
+                        datePublished: '2020-03-10', category: categories[6], studio: studios[2], platform: platforms[0] },
+                     {name: 'Starcraft 2: Wings of Liberty', description: "Combat tripartite dans l'espace.", note: 18.0,
+                        datePublished: '2010-07-27', category: categories[2], studio: studios[1], platform: platforms[0] },
+                     {name: 'The Legend of Zelda: Breath of the Wild', description: 'Sauver Hyrule et la princesse Zelda du Fléau.', note: 20.0,
+                        datePublished: '2017-03-03', category: categories[0], studio: studios[6], platform: platforms[3] },
+                     {name: 'Star Wars Battlefront II', description: 'Combatter dans une galaxie lointaine, très lointaine.', note: 15.5,
+                        datePublished: '2017-11-17', category: categories[11], studio: studios[7], platform: platforms[0] },
+                     {name: 'Star Wars: Squadrons', description: "Combatter tour à tour pour la nouvelle République et les vestiges de l'Empire.", note: 19.0,
+                        datePublished: '2020-10-02', category: categories[10], studio: studios[7], platform: platforms[0] },
+                     {name: 'Warcraft III: Reign of Chaos', description: 'Entrer dans le monde d Azeroth pour combattre pour la Horde ou l Alliance', note: 19.0,
+                        datePublished: '2002-07-05', category: categories[2], studio: studios[1], platform: platforms[0] }
                     ])
-reviews = Review.create([{noteReview: 5, description: 'Super cool', user: users[1], game: games[0]},{noteReview: 4.5, description: "Cette version est la meilleur du jeu", user: users[2], game: games[1]},
-                     {noteReview: 4.5, description: "J'ai bien aimé ce jeu", user: users[1], game: games[2]}, {noteReview: 4.5, description: 'Les bonus sont super', user: users[1], game: games[3]},
-                     {noteReview: 5, description: 'Refonte graphique au top, histoire immersif, musique intense, un jeu en or', user: users[1], game: games[4]}
+
+reviews = Review.create([{noteReview: 20.0, description: 'Super cool', user: users[1], game: games[0]},
+                     {noteReview: 18.0, description: "Cette version est la meilleur du jeu", user: users[2], game: games[1]},
+                     {noteReview: 18.0, description: "J'ai bien aimé ce jeu", user: users[1], game: games[2]},
+                     {noteReview: 18.0, description: 'Les bonus sont super', user: users[1], game: games[3]},
+                     {noteReview: 20.0, description: 'Refonte graphique au top, histoire immersif, musique intense, un jeu en or', user: users[1], game: games[4]},
+                     {noteReview: 15.0, description: "Un jeu super mais trop bugé", user: users[2], game: games[5]},
+                     {noteReview: 15.0, description: "Un jeu sympa", user: users[2], game: games[6]},
+                     {noteReview: 18.0, description: "Pas mal comme jeu de stratégie en temps réel et avec une campagne sympa", user: users[2], game: games[7]},
+                     {noteReview: 20.0, description: "Un des meilleur jeu de la saga", user: users[1], game: games[8]},
+                     {noteReview: 15.5, description: "Un jeu qui s'est amélioré depuis sa sortie mais qui aurais du encore bénéficier de contenu", user: users[2], game: games[9]},
+                     {noteReview: 19.0, description: "J'ai adoré", user: users[1], game: games[10]},
+                     {noteReview: 19.0, description: "Un excellent jeu de stratégie", user: users[2], game: games[11]}
                     ])
